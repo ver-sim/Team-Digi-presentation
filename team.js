1ef69285d5cdddf201e3acb347176cd3bf1770b9
Marco Fonti <fontimarco_2022@libero.it>
2023-10-04 21:18:39 +0200

    filiale locale marco

--- team.js
+++ team.js
@@ -1,1 +1,5 @@
-let team_3 = [];
+let team_3 = [
+
+    {name:"Marco", surname:"Fonti", age: 24, city: "Bologna", hobby: "Calcio",favoriteFood: "Pizza", favoriteVideoGame:"Fifa24", favoriteFilm: "Troy", favoriteBook: "Sherlock Holmes", petName: "Zoe"}
+
+];
