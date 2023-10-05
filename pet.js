fe04a18a0eeb23ea764d6de3d12b6b59b4ed5c52
JackTrem93 <145670788+JackTrem93@users.noreply.github.com>
2023-10-05 11:47:07 +0200

    Update pet.js

--- pet.js
+++ pet.js
@@ -1,6 +1,6 @@
 function pet(array){
     for(let i=0; i< Array.length;i++){
-        if(array[i].animalpet == "")
+        if(array[i].petName == "")
         console.log(array[i].name + "");
     }
 }
