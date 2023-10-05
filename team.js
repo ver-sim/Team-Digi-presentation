8b449104937a230ee5e34abb6141f4d15bb4b22b
JackTrem93 <145670788+JackTrem93@users.noreply.github.com>
2023-10-05 10:45:15 +0200

    Add files via upload

---
+++ team.js
@@ -0,0 +1,3 @@
+let team_3 = [
+    {name: "Giacomo", surname: "Tremulo", age: 30, city: "Sinnai", hobby: "referee", favoriteFood: "sushi", favoriteVideoGame: "HoI4", favoriteFilm: "limitless", favoriteBook: "Il giocatore", petName: ""},
+];
