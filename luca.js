1bb9c80298a2f5cf329ab2e0605b71a9f3bd6ac4
Luca Piantadosi <lu.piantadosi125@gmail.com>
2023-10-05 11:02:29 +0200

    luca

---
+++ luca.js
@@ -0,0 +1,14 @@
+let luca [
+	{
+		name: "Luca",
+		surname: "Piantadosi",
+		age: 27,
+		city: "Napoli",
+		hobby: "Calcio a 5",
+		favoriteFood: "parmigiana di melanzane",
+		favoriteVideoGame: "Valorant",
+		favoriteFilm: "Pulp Fiction",
+		favoriteBook: "L'incubo di Hill House",
+		petName: "nn",
+	}
+];
