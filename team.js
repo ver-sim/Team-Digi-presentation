a1876b5b7700521ee7735724b688de1524eefa03
RobiChiri <robertochiriac8@gmail.com>
2023-10-05 10:51:49 +0200

    branch Roberto

---
+++ team.js
@@ -0,0 +1,14 @@
+let team_3 = [
+	{
+		name: "Roberto",
+		surname: "Chiriac",
+		age: 20,
+		city: "Padova",
+		hobby: "Football Referee",
+		favoriteFood: "pizza",
+		favoriteVideoGame: "The Witcher",
+		favoriteFilm: "Interstellar",
+		favoriteBook: "After",
+		petName: "Puff",
+	}
+];
