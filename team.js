7aa85a1e70fff53aab07747e3973254bc6f3bae6
ver-sim <127152299+ver-sim@users.noreply.github.com>
2023-10-05 11:59:29 +0200

    Update team.js

--- team.js
+++ team.js
@@ -1,16 +1,16 @@
 let team_3 = [
-  {
-    name: "Giacomo", 
-    surname: "Tremulo", 
-    age: 30, 
-    city: "Sinnai", 
-    hobby: "referee", 
-    favoriteFood: "sushi", 
-    favoriteVideoGame: "HoI4", 
-    favoriteFilm: "limitless", 
-    favoriteBook: "Il giocatore", 
-    petName: ""
-  },
+	{
+	    name: "Giacomo", 
+	    surname: "Tremulo", 
+	    age: 30, 
+	    city: "Sinnai", 
+	    hobby: "referee", 
+	    favoriteFood: "sushi", 
+	    favoriteVideoGame: "HoI4", 
+	    favoriteFilm: "limitless", 
+	    favoriteBook: "Il giocatore", 
+	    petName: ""
+	},
   
 	{
 		name: "Roberto",
@@ -24,32 +24,41 @@ let team_3 = [
 		favoriteBook: "After",
 		petName: "Puff"
 	},
+	{
+		name: "Verdiana", 
+		surname: "Simonetti", 
+		age: 29, 
+		city: "Chiusdino", 
+		hobby: "leggere", 
+		favoriteFood: "lasagna", 
+		favoriteVideoGame: "AoM", 
+		favoriteFilm: "guardiani della galassia", 
+		favoriteBook: "honzuki", 
+		petName: "kyuubu"
+	  },
   
-  {
-    name: "Verdiana", 
-    surname: "Simonetti", 
-    age: 29, 
-    city: "Chiusdino", 
-    hobby: "leggere", 
-    favoriteFood: "lasagna", 
-    favoriteVideoGame: "AoM", 
-    favoriteFilm: "guardiani della galassia", 
-    favoriteBook: "honzuki", 
-    petName: "kyuubu"
-  },
-  
-  {
-    name:"Marco", 
-    surname:"Fonti", 
-    age: 24, 
-    city: "Bologna", 
-    hobby: "Calcio", 
-    favoriteFood: "Pizza", 
-    favoriteVideoGame:"Fifa24", 
-    favoriteFilm: "Troy", 
-    favoriteBook: "Sherlock Holmes", 
-    petName: "Zoe"
-  },
-    
-    
+	  {
+		name:"Marco", 
+	    	surname:"Fonti", 
+	    	age: 24, 
+	    	city: "Bologna", 
+	    	hobby: "Calcio", 
+	    	favoriteFood: "Pizza", 
+	    	favoriteVideoGame:"Fifa24", 
+	    	favoriteFilm: "Troy", 
+	    	favoriteBook: "Sherlock Holmes", 
+	    	petName: "Zoe"
+	  },
+	{
+		name: "Luca",
+		surname: "Piantadosi",
+		age: 27,
+		city: "Napoli",
+		hobby: "Calcio a 5",
+		favoriteFood: "parmigiana di melanzane",
+		favoriteVideoGame: "Valorant",
+		favoriteFilm: "Pulp Fiction",
+		favoriteBook: "L'incubo di Hill House",
+		petName: "nn",
+	}  
 ];
